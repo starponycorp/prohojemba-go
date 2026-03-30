8b4e5aaefc1b76bff6119dcc6a23204392b106b7
starponycorp <130343724+starponycorp@users.noreply.github.com>
2026-03-30 17:23:51 +0500

    Create user.go

---
+++ internal/domain/user.go
@@ -0,0 +1,19 @@
+package domain
+
+type Permissions uint8
+
+const (
+	CanEditTitle Permissions = 1 << iota
+	CanEditPublishers
+	CanEditPlatforms
+	CanEditGenres
+	CanEditUsers
+)
+
+type User struct {
+	Id             uint
+	Username       string
+	ViewName       string
+	HashedPassword string
+	Permission     Permissions
+}
